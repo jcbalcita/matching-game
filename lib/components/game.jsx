c2dac42dc9ed5bd24866bda2a3f4e2f4cde7e150
JC Balcita <john.balcita@gmail.com>
2016-12-14 09:58:27 -0800

    creates react components and renders test text to board

---
+++ lib/components/game.jsx
@@ -0,0 +1,17 @@
+// import Board from './board';
+import React from 'react';
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>"At least this is rendering."</div>
+    )
+  }
+
+}
+
+export default Game;
